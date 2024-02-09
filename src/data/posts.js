7e7d199e7b51863bf4e7f2ceb88d0ceeb35538f9
Toyyib Lawal <toyibe25@gmail.com>
2024-02-09 17:36:34 +0100

    posts update

--- src/data/posts.js
+++ src/data/posts.js
@@ -5,18 +5,54 @@ export const posts = [
 	},
 	{
 		name: 'sug_vice_president',
-		label: 'SUG Vice President',
+		label: 'SUG Vice-President',
 	},
 	{
 		name: 'sug_secretary',
 		label: 'SUG Secretary',
 	},
 	{
+		name: 'sug_treasurer',
+		label: 'SUG Treasurer',
+	},
+	{
+		name: 'sug_welfare',
+		label: 'SUG Welfare Director',
+	},
+	{
+		name: 'sug_sport',
+		label: 'SUG Sport Director',
+	},
+	{
+		name: 'sug_food',
+		label: 'SUG Food and Environmental Director',
+	},
+	{
 		name: 'faculty_president',
 		label: 'Faculty President',
 	},
 	{
+		name: 'faculty_vice_president',
+		label: 'Faculty Vice-President',
+	},
+	{
+		name: 'faculty_secretary',
+		label: 'Faculty Secretary',
+	},
+	{
+		name: 'faculty_welfare',
+		label: 'Faculty Welfare Director',
+	},
+	{
+		name: 'faculty_sport',
+		label: 'Faculty Sport Director',
+	},
+	{
+		name: 'faculty_treasurer',
+		label: 'Faculty Treasurer',
+	},
+	{
 		name: 'departmental_rep',
-		label: 'Department Rep',
+		label: 'Departmental Representative',
 	},
 ];
